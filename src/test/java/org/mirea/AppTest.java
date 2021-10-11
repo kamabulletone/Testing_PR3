76005039e480081ce91c002d160748794d9cff8d
Lvov T.R <tin-97@yandex.ru>
2021-10-11 18:42:40 +0300

    added document parsing test

--- src/test/java/org/mirea/AppTest.java
+++ src/test/java/org/mirea/AppTest.java
@@ -1,19 +1,38 @@
 package org.mirea;
 
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
 
-/**
- * Unit test for simple App.
- */
+@ExtendWith(MockitoExtension.class)
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testDocumentContent() throws IOException {
+        String testingHTML = "<div class=\"tablica_stroka\"> \n" +
+                "          <div class=\"holst_gener\" id=\"vov\">\n" +
+                "           <div class=\"blok_otvet\" id=\"bov\" style=\"font-size:60px;\">\n" +
+                "            Крахмал\n" +
+                "           </div>\n" +
+                "          </div> \n" +
+                "         </div> ";
+
+        Document doc = Jsoup.parse(testingHTML);
+        Connection connection = Mockito.mock(Connection.class);
+        Mockito.when(connection.get()).thenReturn(doc);
+
+        Assertions.assertNotNull(connection.get());
+        MatcherAssert.assertThat(connection.get(), Matchers.equalTo(doc));
+
     }
 }
